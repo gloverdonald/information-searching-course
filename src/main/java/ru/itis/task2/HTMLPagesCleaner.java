d1cf91e949068f0a4aa42b180fda5ea599b9428e
m.raymanova <m.raymanova@tinkoff.ru>
2024-02-29 00:14:28 +0300

    add tokenization

--- src/main/java/ru/itis/task2/HTMLPagesCleaner.java
+++ src/main/java/ru/itis/task2/HTMLPagesCleaner.java
@@ -4,6 +4,8 @@ import org.jsoup.Jsoup;
 import ru.itis.Util;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HTMLPagesCleaner {
     private final String pathToFilesDirectory;
@@ -17,16 +19,26 @@ public class HTMLPagesCleaner {
 
     public void generateCleanText() {
         var fileNames = Util.listFilesUsingJavaIO(pathToFilesDirectory);
+        Set<String> tokens = new HashSet<>();
         for (String fileName : fileNames) {
             var html = Util.loadTextFromFile(pathToFilesDirectory.concat("/").concat(fileName));
             var res = Jsoup.parse(html).text();
-            StringTokenizer st = new StringTokenizer(res, " .,0123456789");
+            StringTokenizer st = new StringTokenizer(res, " .,!:;\'\"\\@#$%&*()<>?`~{}[]0123456789");
 
             StringBuilder tokenizedRes = new StringBuilder();
             while (st.hasMoreTokens()) {
                 tokenizedRes.append(st.nextToken()).append("\n");
             }
-            Util.writeToFile(tokenizedRes.toString(), pathToResultFilesDirectory.concat("/").concat(fileName));
+
+            String regex = "[А-ЯЁ]*[а-яё]+";
+            String text = tokenizedRes.toString();
+            Pattern pattern = Pattern.compile(regex, Pattern.UNICODE_CASE);
+            Matcher matcher = pattern.matcher(text);
+            while (matcher.find()) {
+                tokens.add(text.substring(matcher.start(), matcher.end()).toLowerCase());
+            }
         }
+            Util.writeToFile(String.join("\n",tokens), pathToResultFilesDirectory.concat("/")
+                    .concat("tokens.txt"));
     }
 }
