a65125414953b3c0f005fb21e438e5d1ba79b9c1
Milana <20milana02@gmail.com>
2024-03-28 18:11:07 +0300

    task 5

---
+++ src/main/java/ru/itis/task5/VectorSearchMain.java
@@ -0,0 +1,19 @@
+package ru.itis.task5;
+
+import java.util.Scanner;
+
+public class VectorSearchMain {
+    public static void main(String[] args) {
+        VectorSearch vectorSearch = new VectorSearch();
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String query;
+            System.out.print("Запрос: ");
+            query = sc.nextLine();
+            if (query.equals("!exit")) break;
+            vectorSearch.searchPages(query);
+        }
+
+    }
+}
